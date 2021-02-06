f455cfad5ca2f4c4e28814fa44e4111080a7a10c
IshaSingh0228 <ishasingh0228@gmail.com>
2021-02-06 12:45:26 +0530

    created calculation class

---
+++ challenge1/src/challenge1/Calulation.java
@@ -0,0 +1,17 @@
+package challenge1;
+
+public class Calulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=20;
+//addition
+System.out.println("adding two numbers:"+(a+b));
+//subtract
+System.out.println("subtracting:"+(b-a));
+
+
+	}
+
+}
